9944015531694a4b45597f23e3cc6beea9a011e5
unknown <lambilat@protonmail.com>
2023-02-26 21:34:41 +0800

    ge

--- index.js
+++ index.js
@@ -1,39 +1,15 @@
-
-require('dotenv').config()
-
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
 console.log("Welcome")
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
-const { DynamoDBClient, ListTablesCommand } = require("@aws-sdk/client-dynamodb");
-
-
-
-app.use('*', (req, res) => {
-  (async () => {
-    console.log("Trying to Connect AWS Database.")
-    const client = new DynamoDBClient({ region: "ap-southeast-1" });
-    const command = new ListTablesCommand({});
-    try {
-      console.log("AWS Database Connected.")
-      const results = await client.send(command);
-      console.log(results.TableNames.join("\n"));
-      res.json({ msg: results.TableNames.join("\n") }).end()
-    } catch (err) {
-      console.log("AWS Database Failed.")
-      console.error(err);
-    }
-  })();
-
-})
-
-
-
-app.listen(port, () => {
-  console.log(`index.js listening on ${port}`)
-})
+console.log("----------------------------------")
+console.log("Cyclic Default")
+console.log(`Access Key: ${process.env.AWS_ACCESS_KEY_ID}`)
+console.log(`Secret Key: ${process.env.AWS_SECRET_KEY}`)
+console.log("----------------------------------")
+console.log("----------------------------------")
+console.log("My Env")
+console.log(`Access Key: ${process.env.aws_access_key_id}`)
+console.log(`Secret Key: ${process.env.aws_secret_access_key}`)
+console.log("----------------------------------")
