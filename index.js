346a4004b80febacbf28ec3545e8963dda52bd11
JUNJUN <lambilat@protonmail.com>
2023-02-26 19:06:58 +0800

    fix

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+console.log("Welcome")
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 const { DynamoDBClient, ListTablesCommand } = require("@aws-sdk/client-dynamodb");
 
 (async () => {
+  console.log("Trying to Connect AWS Database.")
   const client = new DynamoDBClient({ region: "ap-southeast-1" });
   const command = new ListTablesCommand({});
   try {
@@ -20,6 +23,7 @@ const { DynamoDBClient, ListTablesCommand } = require("@aws-sdk/client-dynamodb"
     console.error(err);
   }
 })();
+
 
 
 app.listen(port, () => {
